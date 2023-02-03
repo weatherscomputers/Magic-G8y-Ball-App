25abe8c1f874f1a0b95de0ba65dab17954c9735e
weatherscomputers <markweathers84@gmail.com>
2023-02-02 21:46:43 -0500

    My first commit Magic G8y Ball Beta 1.0

---
+++ Magic G8y Ball/EightBallImages.swift
@@ -0,0 +1,24 @@
+//
+//  EightBallImages.swift
+//  Magic G8y Ball
+//
+//  Created by Mark Weathers on 2/2/23.
+//
+
+import UIKit
+
+struct EightBallImages {
+    static var images = [
+        //UIImage(named: "Back8Ball_CheckedNo")!,
+        UIImage(named: "Back8Ball_Hmmm")!,
+        //UIImage(named: "Back8Ball_HmmmNo")!,
+        UIImage(named: "Back8Ball_Kindof")!,
+        //UIImage(named: "Back8Ball_SignsNot")!,
+        UIImage(named: "Back8Ball_SignsYes")!,
+        UIImage(named: "Back8Ball_UMM")!,
+        UIImage(named: "Back8Ball_Yes")!,
+        UIImage(named: "Back8Ball_You")!,
+        //UIImage(named: "Back8Ball_No")!
+    ]
+    
+}
---
+++ Magic G8y Ball/Main8BallVC.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Magic G8y Ball
+//
+//  Created by Mark Weathers on 2/2/23.
+//
+//Customer hits rules, pop up happens that say rules and what to say, shakes the phone, answers appear that they are randomly selected but actually always yes gay answers appear
+
+// 1. VC needs to be first responder to the
+// 2. VC needs to be able to become 1st responder
+// 3. Detect Shake
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var eightBallImage: UIImageView!
+    @IBOutlet var eightBallLabel: UILabel!
+    
+    var images: [UIImage] = EightBallImages.images
+    
+    
+    var seconds = 4
+    var timer = Timer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Make 8BallVC first responder
+        becomeFirstResponder()
+        
+    }
+    // by default the view controller is not able to become a first responder so override canBecomeFirstResponder and return true
+    override var canBecomeFirstResponder: Bool{
+        return true
+    }
+    
+    // functions to detect when motion begins and ends when user shakes the phone
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        if motion == .motionShake {
+            
+            sleep(1)
+           
+            self.eightBallImage.image = images.randomElement() ?? UIImage(named: "Back8Ball")
+            self.eightBallLabel.text = "You are Gay shake again if you would like"
+        }
+        
+    }
+    
+}
+
+// Notes:
+//a Responder is first in line to respond to any given event
+// in iOS you have a bunch of objects that can respond to any given event and they are setup in a Responder chain
+// in this case we are making our 8BallVC the first responder in this chain to respond to the given event
